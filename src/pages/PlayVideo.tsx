3cdb5969f53fc4a87a0b01783e896757129ec80d
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-12 02:16:01 +0500

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -115,7 +115,7 @@ export function PlayVideo() {
 
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://sorrowfulpsychology.com/HE9TFh'
+    'https://bmadss.com/get/?spot_id=2026313&cat=25&subid=1576043020'
   ];
 
   const shuffleArray = (array: any[]) => {
